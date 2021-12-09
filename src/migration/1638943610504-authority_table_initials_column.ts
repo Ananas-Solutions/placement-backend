4f14a7b8a9a4276316ac17dd8879e3e0da26f0a6
Bibash Pokharel <bibashpokharel7@gmail.com>
2021-12-09 18:27:51 +0545

    added semester, college department, cloudinary and department units

--- src/migration/1638943610504-authority_table_initials_column.ts
+++ src/migration/1638943610504-authority_table_initials_column.ts
@@ -1,14 +1,23 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class authorityTableInitialsColumn1638943610504 implements MigrationInterface {
-    name = 'authorityTableInitialsColumn1638943610504'
+export class authorityTableInitialsColumn1638943610504
+  implements MigrationInterface
+{
+  name = 'authorityTableInitialsColumn1638943610504';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "authority" ALTER COLUMN "initials" SET NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "authority" ADD "initials" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "authority" ALTER COLUMN "initials" SET NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "authority" ALTER COLUMN "initials" DROP NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "authority" ALTER COLUMN "initials" DROP NOT NULL`,
+    );
+    await queryRunner.query(`ALTER TABLE "authority" DROP COLUMN "initials"`);
+  }
 }
