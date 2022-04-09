970acc99fb10162305e31463a8a1fa9dfd7437af
Bibash Pokharel <bibashpokharel7@gmail.com>
2022-04-09 10:52:31 +0545

    added the sort operation in find all users

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { User } from './entity/user.entity';
 import { UserService } from './user.service';
+import { StudentProfile } from 'src/student/entity/student-profile.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, StudentProfile])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
