17ac8012e16b319731656fc874f6e2996990c0ca
Bibash Pokharel <bibashpokharel7@gmail.com>
2023-03-27 13:10:36 +0545

    added guard for fetching semester

--- src/modules/semester/semester.controller.ts
+++ src/modules/semester/semester.controller.ts
@@ -22,31 +22,35 @@ import { SemesterService } from './semester.service';
 @ApiTags('semester')
 @UseInterceptors(ErrorInterceptor)
 @UseGuards(JwtAuthGuard, RolesGuard)
-@Roles(UserRoleEnum.ADMIN)
 @Controller('semester')
 export class SemesterController {
   constructor(private readonly semesterService: SemesterService) {}
 
+  @Roles(UserRoleEnum.ADMIN)
   @Post()
   async createSemester(@Body() body: SemesterDto) {
     return await this.semesterService.save(body);
   }
 
+  @Roles(UserRoleEnum.ADMIN, UserRoleEnum.CLINICAL_COORDINATOR)
   @Get(':id')
   async getOneSemester(@Param('id') id: string) {
     return await this.semesterService.findOne(id);
   }
 
+  @Roles(UserRoleEnum.ADMIN, UserRoleEnum.CLINICAL_COORDINATOR)
   @Get()
   async getAllSemester() {
     return await this.semesterService.findAll();
   }
 
+  @Roles(UserRoleEnum.ADMIN)
   @Put(':id')
   async updateSemester(@Param('id') id: string, @Body() body: SemesterDto) {
     return await this.semesterService.update(id, body);
   }
 
+  @Roles(UserRoleEnum.ADMIN)
   @Delete(':id')
   async deleteSemester(@Param('id') id: string) {
     return await this.semesterService.delete(id);
