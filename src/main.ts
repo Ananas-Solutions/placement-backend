5d6e72980c2a054469e3e957d674ea3080bf60c0
Bibash Pokharel <bibashpokharel7@gmail.com>
2024-04-07 22:01:05 +0545

    updated logic on transfer and shuffle

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://uos.myplacement.me'],
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['https://uos.myplacement.me'],
+    },
   });
+
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   app.useGlobalPipes(new ValidationPipe());
   // app.use(cookieParser(process.env.COOKIE_SECRET));
