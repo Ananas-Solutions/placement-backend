bd1da6a1efa5394eebdc354e9760dc6b5cdb2720
Bibash Pokharel <bibashpokharel7@gmail.com>
2024-04-07 22:22:18 +0545

    updated logic on transfer and shuffle

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: ['https://uos.myplacement.me', 'http://localhost:6768'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    },
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://uos.myplacement.me', 'http://localhost:6768'], // Set the allowed origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Set the allowed HTTP methods
   });
 
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
