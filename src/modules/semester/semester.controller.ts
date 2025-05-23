527ea9d7d8170fb264a406aeb3a3fd5b1c9e6d17
Bibash Pokharel <bibashpokharel7@gmail.com>
2025-05-23 11:38:23 +0545

    fixed response on semester API

--- src/modules/semester/semester.controller.ts
+++ src/modules/semester/semester.controller.ts
@@ -20,6 +20,10 @@ import { ErrorInterceptor } from 'interceptor/error.interceptor';
 import { SemesterDto } from './dto';
 import { SemesterService } from './semester.service';
 import { SearchQueryDto } from 'commons/dto';
+import { TransformResponseInterceptor } from 'interceptor/transform-response.interceptor';
+import { SemesterResponse } from './response';
+import { SuccessMessageResponse } from 'commons/response';
+import { SimplifiedSemesterResponse } from './response/simplified-semester.response';
 
 @ApiTags('semester')
 @UseInterceptors(ErrorInterceptor)
@@ -30,30 +34,35 @@ export class SemesterController {
 
   @Roles(UserRoleEnum.ADMIN)
   @Post()
+  @UseInterceptors(new TransformResponseInterceptor(SemesterResponse))
   async createSemester(@Body() body: SemesterDto) {
     return await this.semesterService.save(body);
   }
 
   @Roles(UserRoleEnum.ADMIN, UserRoleEnum.CLINICAL_COORDINATOR)
   @Get(':id')
+  @UseInterceptors(new TransformResponseInterceptor(SimplifiedSemesterResponse))
   async getOneSemester(@Param('id') id: string) {
     return await this.semesterService.findOne(id);
   }
 
-  @Roles(UserRoleEnum.ADMIN, UserRoleEnum.CLINICAL_COORDINATOR)
   @Get()
+  @Roles(UserRoleEnum.ADMIN, UserRoleEnum.CLINICAL_COORDINATOR)
+  @UseInterceptors(new TransformResponseInterceptor(SemesterResponse))
   async getAllSemester(@Query() query: SearchQueryDto) {
     return await this.semesterService.findAll(query);
   }
 
   @Roles(UserRoleEnum.ADMIN)
   @Put(':id')
+  @UseInterceptors(new TransformResponseInterceptor(SemesterResponse))
   async updateSemester(@Param('id') id: string, @Body() body: SemesterDto) {
     return await this.semesterService.update(id, body);
   }
 
   @Roles(UserRoleEnum.ADMIN)
   @Delete(':id')
+  @UseInterceptors(new TransformResponseInterceptor(SuccessMessageResponse))
   async deleteSemester(@Param('id') id: string) {
     return await this.semesterService.delete(id);
   }
