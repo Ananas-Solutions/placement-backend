bef29e18647a53163d4f34a4e8d6783080a4e971
Bibash Pokharel <bibashpokharel7@gmail.com>
2025-06-10 12:05:59 +0400

    updated department controller to query using post

--- src/modules/department/dto/query-hospital-department.dto.ts
+++ src/modules/department/dto/query-hospital-department.dto.ts
@@ -1,9 +1,8 @@
-import { ArrayMinSize, IsArray, IsOptional, IsString } from 'class-validator';
+import { ArrayMinSize, IsArray, IsOptional } from 'class-validator';
 import { SearchQueryDto } from 'commons/dto';
 
 export class QueryHospitalDepartmentDto extends SearchQueryDto {
   @IsArray()
-  @IsString({ each: true })
   @IsOptional()
   @ArrayMinSize(1)
   hospitalIds: string[];
